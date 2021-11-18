6fe7d5cd2d68e6cdc8928b18f640cc2c0727e7e8
EduardoReisUX <ducabruno09@hotmail.com>
2021-11-18 15:36:26 -0400

    style: add entire home page styled

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./styles";
+import entrada from "../../assets/entradas.svg";
+import saida from "../../assets/saídas.svg";
+import total from "../../assets/total.svg";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={entrada} alt="entrada" />
+        </header>
+        <strong>R$1000</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saida</p>
+          <img src={saida} alt="saida" />
+        </header>
+        <strong>- R$1000</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={total} alt="total" />
+        </header>
+        <strong>R$1000</strong>
+      </div>
+    </Container>
+  );
+}
